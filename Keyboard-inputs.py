7304fb9a3d54bbb06ebe64be14ff05863eb8443c
krishna kishore <144976947+mnbvcxzlkhj@users.noreply.github.com>
2024-12-13 08:11:39 +0530

    Add files via upload

---
+++ Keyboard-inputs.py
@@ -0,0 +1,6 @@
+a = float(input("Enter coefficient a: "))
+b = float(input("Enter coefficient b: "))
+c = float(input("Enter coefficient c: "))
+x = float(input("Enter value of x: "))
+y = a * x**2 + b * x + c
+print(f"For x={x}, y={y}")
